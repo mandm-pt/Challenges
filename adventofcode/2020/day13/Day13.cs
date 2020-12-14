98908e1ff71a427318698c6f4f4d10a554dae3b9
mandm-pt <MFR_Morais@hotmail.com>
2020-12-14 01:35:01 +0000

    attempt 2020 / 13 part 2

--- adventofcode/2020/day13/Day13.cs
+++ adventofcode/2020/day13/Day13.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace AoC.Solutions._2020
@@ -16,7 +17,12 @@ namespace AoC.Solutions._2020
         {
             await base.LoadyAsync();
 
-            //inputLines = new[] { "939", "7,13,x,x,59,x,31,19" };
+            inputLines = new[] { "1068781", "7,13,x,x,59,x,31,19" };
+            //inputLines = new[] { "3417", "17,x,13,19" };
+            //inputLines = new[] { "754018", "67,7,59,61" };
+            //inputLines = new[] { "779210", "67,x,7,59,61" };
+            //inputLines = new[] { "1261476", "67,7,x,59,61" };
+            //inputLines = new[] { "1202161486", "1789,37,47,1889" };
 
             EarliestTimestamp = long.Parse(inputLines[0]);
             Buses = inputLines[1]
@@ -26,20 +32,75 @@ namespace AoC.Solutions._2020
                 .ToArray();
         }
 
-        protected override Task<string> Part1Async()
+        ////protected override Task<string> Part1Async()
+        //{
+        //    var bus = Buses
+        //        .Select(b => new
+        //        {
+        //            Id = b,
+        //            NextTime = (EarliestTimestamp / b) * b + b
+        //        })
+        //        .OrderBy(b => b.NextTime)
+        //        .First();
+
+        //    long result = (bus.NextTime - EarliestTimestamp) * bus.Id;
+
+        //    return Task.FromResult(result.ToString());
+        //}
+
+        protected override Task<string> Part2Async()
         {
-            var bus = Buses
-                .Select(b => new
+            string[]? rawBuses = inputLines[1].Split(',');
+            var bus = new List<BusTime>();
+            for (int i = 0; i < rawBuses.Length; i++)
+            {
+                if (rawBuses[i] == "x")
+                    continue;
+
+                bus.Add(new(int.Parse(rawBuses[i]), i, 0));
+            }
+            bus.Add(new(1, 1, 1));
+
+            int busIdx = 0;
+            long timestamp = 1068780; // 100133482813506;
+            do
+            {
+                if (busIdx == 0)
                 {
-                    Id = b,
-                    NextTime = (EarliestTimestamp / b) * b + b
-                })
-                .OrderBy(b => b.NextTime)
-                .First();
+                    bus[busIdx].StartTimestamp = timestamp = NextMultiple(timestamp, bus[busIdx].Id);
+                    busIdx++;
+                }
+                else
+                {
+                    //timestamp = NextMultiple(bus[0].StartTimestamp, bus[busIdx].Id);
+                    timestamp += bus[0].StartTimestamp + bus[busIdx].Index - timestamp;
 
-            var result = (bus.NextTime - EarliestTimestamp) * bus.Id;
+                    if (bus[0].StartTimestamp + bus[busIdx].Index == timestamp)
+                        busIdx++;
+                    else
+                        busIdx = 0;
+                }
 
-            return Task.FromResult(result.ToString());
+            } while (busIdx <= bus.Count - 1);
+
+            return Task.FromResult((bus[0].StartTimestamp.ToString() == inputLines[0]).ToString());
+        }
+
+        private static long NextMultiple(long n, long m)
+            => n + (m - n % m);
+
+        private class BusTime
+        {
+            public BusTime(int id, int index, long startTimestamp)
+            {
+                Id = id;
+                Index = index;
+                StartTimestamp = startTimestamp;
+            }
+
+            public int Id { get; }
+            public int Index { get; }
+            public long StartTimestamp { get; set; }
         }
     }
 }
