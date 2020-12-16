57adc68dd7ba186be52fa3f9db6f76f7f995954d
mandm-pt <MFR_Morais@hotmail.com>
2020-12-16 11:54:03 +0000

    add 2020 / 16 part 2

--- adventofcode/2020/day16/Day16.cs
+++ adventofcode/2020/day16/Day16.cs
@@ -17,7 +17,7 @@ namespace AoC.Solutions._2020
         private readonly Regex CaptureTicketsRegex = new Regex(@"^[(\d+),]+", RegexOptions.Compiled | RegexOptions.Multiline);
 
         private Field[] Fields = Array.Empty<Field>();
-        private int[][] Tickets = Array.Empty<int[]>();
+        private long[][] Tickets = Array.Empty<long[]>();
 
         protected override async Task LoadyAsync()
         {
@@ -26,43 +26,103 @@ namespace AoC.Solutions._2020
             Fields = CaptureValidNumbersRegex.Matches(contents)
                                 .Select(m =>
                                 {
-                                    int from = int.Parse(m.Groups[2].Value);
-                                    int to = int.Parse(m.Groups[3].Value) - from + 1;
+                                    var from = int.Parse(m.Groups[2].Value);
+                                    var to = int.Parse(m.Groups[3].Value) - from + 1;
 
-                                    int orFrom = int.Parse(m.Groups[4].Value);
-                                    int orTo = int.Parse(m.Groups[5].Value) - orFrom + 1;
+                                    var orFrom = int.Parse(m.Groups[4].Value);
+                                    var orTo = int.Parse(m.Groups[5].Value) - orFrom + 1;
 
-                                    var validNumbers = Enumerable.Range(from, to)
-                                                        .Concat(Enumerable.Range(orFrom, orTo));
+                                    var validNumbers = Enumerable.Range(from, to).Select(n => (long)n).ToList()
+                                                        .Concat(Enumerable.Range(orFrom, orTo).Select(n => (long)n));
 
-                                    return new Field(m.Groups[1].Value, validNumbers);
+                                    return new Field(m.Groups[1].Value, validNumbers.Cast<long>());
                                 })
                                 .ToArray();
 
             Tickets = CaptureTicketsRegex.Matches(contents)
-                                .Select(m => m.Value.Split(',').Select(int.Parse).ToArray())
+                                .Select(m => m.Value.Split(',').Select(long.Parse).ToArray())
                                 .ToArray();
         }
 
         protected override Task<string> Part1Async()
         {
-            int[] allValidNumbers = Fields.SelectMany(f => f.ValidNumbers).ToArray();
+            var allValidNumbers = Fields.SelectMany(f => f.ValidNumbers).ToArray();
             var allInvalidTickets = FilterTickets(Tickets.Skip(1), t => t.Any(n => !allValidNumbers.Contains(n)));
 
-            int sumInvalid = 0;
-            foreach (int[] ticket in allInvalidTickets)
+            long sumInvalid = 0;
+            foreach (var ticket in allInvalidTickets)
             {
-                sumInvalid += ticket.Where(n => !allValidNumbers.Contains(n)).Aggregate(0, (a, b) => a + b);
+                sumInvalid += ticket.Where(n => !allValidNumbers.Contains(n)).Aggregate((long)0, (a, b) => a + b);
             }
 
             return Task.FromResult(sumInvalid.ToString());
         }
 
-        protected IEnumerable<int[]> FilterTickets(IEnumerable<int[]> tickets, Func<int[], bool> ticketsFilter)
+        protected override Task<string> Part2Async()
         {
-            var filteredTickets = new List<int[]>();
+            var allValidNumbers = Fields.SelectMany(f => f.ValidNumbers).ToArray();
+            var allValidTickets = FilterTickets(Tickets, t => t.All(n => allValidNumbers.Contains(n))).ToArray();
 
-            foreach (int[] ticket in tickets)
+            for (int i = 0; i < allValidTickets.Length; i++)
+            {
+                var ticket = allValidTickets[i];
+
+                for (int j = 0; j < ticket.Length; j++)
+                {
+                    var number = ticket[j];
+
+                    foreach (var field in Fields)
+                    {
+                        if (field.ValidNumbers.Contains(number))
+                        {
+                            if (field.InvalidPositions.Contains(j))
+                                _ = field.ValidPositions.Remove(j);
+                            else if (!field.ValidPositions.Contains(j))
+                                field.ValidPositions.Add(j);
+                        }
+                        else if (!field.InvalidPositions.Contains(j))
+                        {
+                            field.InvalidPositions.Add(j);
+
+                            if (field.ValidPositions.Contains(j))
+                                _ = field.ValidPositions.Remove(j);
+                        }
+                    }
+                }
+            }
+
+            do
+            {
+                foreach (var field in Fields.Where(f => f.ValidPositions.Count == 1))
+                {
+                    var number = field.ValidPositions[0];
+
+                    foreach (var filedToRemove in Fields
+                                                .Where(f => f != field)
+                                                .Where(f => f.ValidPositions.Contains(number)))
+                    {
+                        filedToRemove.ValidPositions.Remove(number);
+                    }
+                }
+            } while (!Fields.All(f => f.ValidPositions.Count == 1));
+
+            var departureFields = Fields.Where(f => f.Name.Contains("departure")).Select(f => f.ValidPositions[0]);
+
+            long[] myTicket = allValidTickets[0];
+            long result = 1;
+            foreach (int idx in departureFields)
+            {
+                result *= myTicket[idx];
+            }
+
+            return Task.FromResult(result.ToString());
+        }
+
+        protected IEnumerable<long[]> FilterTickets(IEnumerable<long[]> tickets, Func<long[], bool> ticketsFilter)
+        {
+            var filteredTickets = new List<long[]>();
+
+            foreach (var ticket in tickets)
             {
                 if (ticketsFilter(ticket))
                     filteredTickets.Add(ticket);
@@ -71,6 +131,13 @@ namespace AoC.Solutions._2020
             return filteredTickets.ToArray();
         }
 
-        private record Field(string Name, IEnumerable<int> ValidNumbers);
+        private record Field(string Name, IEnumerable<long> ValidNumbers)
+        {
+            private readonly IList<int> validPositions = new List<int>();
+            private readonly IList<int> invalidPositions = new List<int>();
+
+            public IList<int> ValidPositions => validPositions;
+            public IList<int> InvalidPositions => invalidPositions;
+        }
     }
 }
