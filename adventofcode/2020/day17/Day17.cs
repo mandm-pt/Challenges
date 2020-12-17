26ada0eeda9b2ef1e813534056e02375b8681cb5
mandm-pt <MFR_Morais@hotmail.com>
2020-12-17 19:59:03 +0000

    add 2020 / 17

--- adventofcode/2020/day17/Day17.cs
+++ adventofcode/2020/day17/Day17.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AoC.Solutions._2020
 {
@@ -9,84 +12,180 @@ namespace AoC.Solutions._2020
         public override int Day => 17;
 
         private const char ActiveChar = '#';
-        private const int MaxGridSize = 20;
-        private bool[,,] Cube = new bool[MaxGridSize, MaxGridSize, MaxGridSize];
+        private Cube3Position[][] Universe = Array.Empty<Cube3Position[]>();
 
         protected override async Task LoadyAsync()
         {
             await base.LoadyAsync();
 
-            Cube = new bool[MaxGridSize, MaxGridSize, MaxGridSize];
+            var newDimension = new List<Cube3Position>();
 
-            for (int x = 0; x < inputLines.Length; x++)
+            for (int y = 0; y < inputLines.Length; y++)
             {
-                for (int y = 0; y < inputLines[x].Length; y++)
+                for (int x = 0; x < inputLines[y].Length; x++)
                 {
-                    Cube[y + 5, x + 5, 10] = inputLines[x][y] == ActiveChar;
+                    if (inputLines[y][x] == ActiveChar)
+                    {
+                        newDimension.Add(new(x, y, 0));
+                    }
                 }
             }
+
+            Universe = new[] { newDimension.ToArray() };
         }
 
         protected override Task<string> Part1Async()
         {
             int totalActive = 0;
-            bool[,,] grid = Cube;
             int count = 0;
 
             while (count++ < 6)
             {
-                (grid, totalActive) = RunCycle(grid);
+                (Universe, totalActive) = RunCycle(Universe);
             }
 
             return Task.FromResult(totalActive.ToString());
         }
 
-        private static (bool[,,], int) RunCycle(bool[,,] grid)
+        protected override async Task<string> Part2Async()
+        {
+            await LoadyAsync();
+
+            Cube4Position[][] newUniverse = Universe
+                                                .Select(u => u
+                                                            .Select(p => new Cube4Position(p.X, p.Y, 0, 0))
+                                                            .ToArray())
+                                                .ToArray();
+
+            int totalActive = 0;
+            int count = 0;
+
+            while (count++ < 6)
+            {
+                (newUniverse, totalActive) = RunCycle(newUniverse);
+            }
+
+            return totalActive.ToString();
+        }
+
+
+        private static (Cube3Position[][], int) RunCycle(Cube3Position[][] dimensions)
         {
             int totalActive = 0;
 
-            bool[,,] newGrid = new bool[MaxGridSize, MaxGridSize, MaxGridSize];
+            int startX = dimensions.SelectMany(d => d).Min(c => c.X) - 1;
+            int startY = dimensions.SelectMany(d => d).Min(c => c.Y) - 1;
+            int startZ = dimensions.SelectMany(d => d).Min(c => c.Z) - 1;
 
-            for (int x = 0; x < MaxGridSize; x++)
+            int endX = dimensions.SelectMany(d => d).Max(c => c.X) + 1;
+            int endY = dimensions.SelectMany(d => d).Max(c => c.Y) + 1;
+            int endZ = dimensions.SelectMany(d => d).Max(c => c.Z) + 1;
+
+            var newj = new List<Cube3Position[]>();
+
+            for (int z = startZ; z <= endZ; z++)
             {
-                for (int y = 0; y < MaxGridSize; y++)
+                for (int y = startY; y <= endY; y++)
                 {
-                    for (int z = 0; z < MaxGridSize; z++)
-                    {
-                        int activeNeighbours = GetNumberOfActiveNeighbours(grid, x, y, z);
+                    var newDimension = new List<Cube3Position>();
 
-                        if (grid[x, y, z] && (activeNeighbours == 2 || activeNeighbours == 3))
+                    for (int x = startX; x <= endX; x++)
+                    {
+                        int activeNeighbours = GetNumberOfActiveNeighbours(dimensions
+                                                    .SelectMany(d => d)
+                                                    .ToArray()
+                                                , x, y, z);
+
+                        bool cubeIsActive = dimensions.SelectMany(d => d).Any(c => c.X == x && c.Y == y && c.Z == z);
+
+                        if (cubeIsActive && (activeNeighbours == 2 || activeNeighbours == 3))
                         {
                             totalActive++;
-                            newGrid[x, y, z] = true;
+                            newDimension.Add(new(x, y, z));
                         }
-                        else if (!grid[x, y, z] && activeNeighbours == 3)
+                        else if (!cubeIsActive && activeNeighbours == 3)
                         {
                             totalActive++;
-                            newGrid[x, y, z] = true;
+                            newDimension.Add(new(x, y, z));
                         }
+                    }
+
+                    if (newDimension.Any())
+                        newj.Add(newDimension.ToArray());
+                }
+            }
+
+            return (newj.ToArray(), totalActive);
+        }
+
+        private static (Cube4Position[][], int) RunCycle(Cube4Position[][] dimensions)
+        {
+            int totalActive = 0;
+
+            int startX = dimensions.SelectMany(d => d).Min(c => c.X) - 1;
+            int startY = dimensions.SelectMany(d => d).Min(c => c.Y) - 1;
+            int startZ = dimensions.SelectMany(d => d).Min(c => c.Z) - 1;
+            int startA = dimensions.SelectMany(d => d).Min(c => c.A) - 1;
+
+            int endX = dimensions.SelectMany(d => d).Max(c => c.X) + 1;
+            int endY = dimensions.SelectMany(d => d).Max(c => c.Y) + 1;
+            int endZ = dimensions.SelectMany(d => d).Max(c => c.Z) + 1;
+            int endA = dimensions.SelectMany(d => d).Max(c => c.A) + 1;
+
+            var newj = new List<Cube4Position[]>();
+
+            for (int a = startA; a <= endA; a++)
+            {
+                for (int z = startZ; z <= endZ; z++)
+                {
+                    for (int y = startY; y <= endY; y++)
+                    {
+                        var newDimension = new List<Cube4Position>();
+
+                        for (int x = startX; x <= endX; x++)
+                        {
+                            int activeNeighbours = GetNumberOfActiveNeighbours(dimensions
+                                                        .SelectMany(d => d)
+                                                        .ToArray()
+                                                    , x, y, z, a);
+
+                            bool cubeIsActive = dimensions.SelectMany(d => d).Any(c => c.X == x && c.Y == y && c.Z == z && c.A == a);
+
+                            if (cubeIsActive && (activeNeighbours == 2 || activeNeighbours == 3))
+                            {
+                                totalActive++;
+                                newDimension.Add(new(x, y, z, a));
+                            }
+                            else if (!cubeIsActive && activeNeighbours == 3)
+                            {
+                                totalActive++;
+                                newDimension.Add(new(x, y, z, a));
+                            }
+                        }
+
+                        if (newDimension.Any())
+                            newj.Add(newDimension.ToArray());
                     }
                 }
             }
 
-            return (grid, totalActive);
+            return (newj.ToArray(), totalActive);
         }
 
-        private static int GetNumberOfActiveNeighbours(bool[,,] grid, int x, int y, int z)
+        private static int GetNumberOfActiveNeighbours(Cube3Position[] cubes, int x, int y, int z)
         {
             int count = 0;
 
-            for (int xn = x - 1; xn < x + 1; xn++)
+            for (int zn = z - 1; zn <= z + 1; zn++)
             {
-                for (int yn = y - 1; yn < y + 1; yn++)
+                for (int yn = y - 1; yn <= y + 1; yn++)
                 {
-                    for (int zn = z - 1; zn < z + 1; zn++)
+                    for (int xn = x - 1; xn <= x + 1; xn++)
                     {
                         if (xn == x && yn == y && zn == z)
                             continue; // skip itself
 
-                        if (PositionInsideBound(xn) && PositionInsideBound(yn) && PositionInsideBound(zn) &&
-                            grid[xn, yn, zn])
+                        if (cubes.Any(c => c.X == xn && c.Y == yn && c.Z == zn))
                         {
                             count++;
                         }
@@ -97,6 +196,83 @@ namespace AoC.Solutions._2020
             return count;
         }
 
-        private static bool PositionInsideBound(int pos) => pos >= 0 && pos < MaxGridSize;
+        private static int GetNumberOfActiveNeighbours(Cube4Position[] cubes, int x, int y, int z, int a)
+        {
+            int count = 0;
+
+            for (int an = a - 1; an <= a + 1; an++)
+            {
+                for (int zn = z - 1; zn <= z + 1; zn++)
+                {
+                    for (int yn = y - 1; yn <= y + 1; yn++)
+                    {
+                        for (int xn = x - 1; xn <= x + 1; xn++)
+                        {
+                            if (xn == x && yn == y && zn == z && an == a)
+                                continue; // skip itself
+
+                            if (cubes.Any(c => c.X == xn && 
+                                               c.Y == yn && 
+                                               c.Z == zn && 
+                                               c.A == an))
+                            {
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        private class Cube3Position
+        {
+            public Cube3Position(int x, int y, int z)
+            {
+                X = x;
+                Y = y;
+                Z = z;
+            }
+
+            public int X { get; }
+            public int Y { get; }
+            public int Z { get; }
+
+            public static bool operator ==(Cube3Position a, Cube3Position b)
+                => a.Equals(b);
+
+            public static bool operator !=(Cube3Position a, Cube3Position b)
+                => !a.Equals(b);
+
+            public override bool Equals(object? obj)
+            {
+                if (obj == null || obj is not Cube3Position)
+                    return false;
+
+                var other = (Cube3Position)obj;
+
+                return X == other.X
+                    && X == other.Y
+                    && X == other.Z;
+            }
+
+            public override int GetHashCode() => base.GetHashCode();
+
+            public override string ToString() => $"X={X} Y={Y} Z={Z}";
+        }
+
+        private class Cube4Position : Cube3Position
+        {
+            public Cube4Position(int x, int y, int z, int a)
+                : base(x, y, z)
+            {
+                A = a;
+            }
+
+            public int A { get; }
+
+            public override string ToString() => $"X={X} Y={Y} Z={Z} A={A}";
+        }
     }
 }
