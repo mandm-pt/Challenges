9cdcca97263f8b256aed60634e670279605743d9
Michel Morais <michel.morais@talkdesk.com>
2021-12-08 15:30:19 +0000

    Add 2021/08

---
+++ adventofcode/2021/08/main.go
@@ -0,0 +1,214 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type DigitFlag int
+
+const (
+	None DigitFlag = 0
+	A    DigitFlag = 1 << iota
+	B    DigitFlag = 2 << iota
+	C    DigitFlag = 4 << iota
+	D    DigitFlag = 8 << iota
+	E    DigitFlag = 16 << iota
+	F    DigitFlag = 32 << iota
+	G    DigitFlag = 64 << iota
+)
+
+type display struct {
+	digitPatterns []string
+	outputDigits  []string
+}
+
+func (this DigitFlag) hasFlag(flag DigitFlag) bool {
+	return this|flag == this
+}
+
+func wiresToDigitFlags(wires string) DigitFlag {
+	flag := None
+
+	for i := range wires {
+		switch wires[i] {
+		case 'a':
+			flag |= A
+		case 'b':
+			flag |= B
+		case 'c':
+			flag |= C
+		case 'd':
+			flag |= D
+		case 'e':
+			flag |= E
+		case 'f':
+			flag |= F
+		case 'g':
+			flag |= G
+		}
+	}
+	return flag
+}
+
+func sumNumbers(displays []string) int {
+	sum := 0
+
+	for _, display := range displays {
+		number, _ := strconv.Atoi(display)
+		sum += number
+	}
+
+	return sum
+}
+
+func countNumbers(displays []string, valuesToCount ...string) int {
+	count := 0
+
+	for _, display := range displays {
+		for _, digit := range display {
+			for _, v := range valuesToCount {
+				if string(digit) == v {
+					count++
+				}
+			}
+		}
+	}
+
+	return count
+}
+
+func decodeOutputDigit(digitPatterns []DigitFlag, digitWires string) int {
+	wireFlags := wiresToDigitFlags(digitWires)
+
+	for i, p := range digitPatterns {
+		if p == wireFlags {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func decodePattern(patterns []string) []DigitFlag {
+	flags := make([]DigitFlag, 10)
+	indexToSolve := []int{}
+
+	for i, p := range patterns {
+		if len := len(p); (len >= 2 && len <= 4) || len == 7 {
+			switch len {
+			case 2: // number 1
+				flags[1] = wiresToDigitFlags(p)
+			case 4: // number 4
+				flags[4] = wiresToDigitFlags(p)
+			case 3: // number 7
+				flags[7] = wiresToDigitFlags(p)
+			case 7: // number 8
+				flags[8] = wiresToDigitFlags(p)
+			}
+		} else {
+			indexToSolve = append(indexToSolve, i)
+		}
+	}
+
+	for _, indexValue := range indexToSolve {
+		pattern := patterns[indexValue]
+		unknownDigitFlags := wiresToDigitFlags(pattern)
+
+		if len := len(pattern); len == 5 { // len 5 possible numbers: 2, 3, 5
+			if unknownDigitFlags.hasFlag(flags[7]) {
+				flags[3] = unknownDigitFlags // number 3
+			} else if unknownDigitFlags.hasFlag(flags[7] ^ flags[4]) {
+				flags[5] = unknownDigitFlags // number 5
+			} else {
+				flags[2] = unknownDigitFlags // number 2
+			}
+		} else { // len 6 possible numbers: 0, 6 9
+			if unknownDigitFlags.hasFlag(flags[4]) && unknownDigitFlags.hasFlag(flags[7]) {
+				flags[9] = unknownDigitFlags // number 9
+			} else if unknownDigitFlags.hasFlag(flags[1]) {
+				flags[0] = unknownDigitFlags // number 0
+			} else {
+				flags[6] = unknownDigitFlags // number 6
+			}
+		}
+	}
+
+	return flags
+}
+
+func fixDisplay(d display) string {
+
+	digitsPatterns := decodePattern(d.digitPatterns)
+
+	displayDigits := ""
+	for _, digitWires := range d.outputDigits {
+		digit := decodeOutputDigit(digitsPatterns, digitWires)
+		displayDigits += strconv.Itoa(digit)
+	}
+
+	return displayDigits
+}
+
+func part2(displays []display) int {
+	fixedDisplays := []string{}
+
+	for _, q := range displays {
+		fixedDisplays = append(fixedDisplays, fixDisplay(q))
+	}
+
+	return sumNumbers(fixedDisplays)
+}
+
+func part1(displays []display) int {
+
+	fixedDisplays := []string{}
+
+	for _, q := range displays {
+		fixedDisplays = append(fixedDisplays, fixDisplay(q))
+	}
+
+	return countNumbers(fixedDisplays, "1", "4", "7", "8")
+}
+
+func getInput(filePath string) ([]display, error) {
+	displays := []display{}
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return displays, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		patternsAndDigits := strings.Split(scanner.Text(), " | ")
+
+		displays = append(displays, display{
+			digitPatterns: strings.Fields(patternsAndDigits[0]),
+			outputDigits:  strings.Fields(patternsAndDigits[1]),
+		})
+	}
+
+	return displays, nil
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Please, add input file path as parameter")
+		os.Exit(1)
+	}
+
+	displays, err := getInput(os.Args[1])
+
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	fmt.Println("P1: ", part1(displays))
+	fmt.Println("P2: ", part2(displays))
+}
