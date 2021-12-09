b8af272eb87b8ec0b8bb2f99337f3cbde287fcb7
Michel Morais <michel.morais@talkdesk.com>
2021-12-09 18:42:31 +0000

    Add 2021/09 part 2

--- adventofcode/2021/09/main.go
+++ adventofcode/2021/09/main.go
@@ -4,20 +4,28 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 )
 
-func getRiskLevel(values []int) int {
+type point struct {
+	x          int
+	y          int
+	value      int
+	neighbours []point
+}
+
+func getRiskLevel(values []point) int {
 	sum := 0
 	for i := range values {
-		sum += values[i] + 1
+		sum += values[i].value + 1
 	}
 
 	return sum
 }
 
-func isLowPoint(point int, neighbours []int) bool {
-	for _, v := range neighbours {
-		if point >= v {
+func isLowPoint(pointValue int, neighbours []point) bool {
+	for _, n := range neighbours {
+		if pointValue >= n.value {
 			return false
 		}
 	}
@@ -25,52 +33,122 @@ func isLowPoint(point int, neighbours []int) bool {
 	return true
 }
 
-func getNeighbours(heightmap [][]int, y, x int) []int {
-	neighbours := []int{}
-	height := len(heightmap)
-	width := len(heightmap[0])
+func getNeighbours(heightmap *[][]int, y, x int) []point {
+	neighbours := []point{}
+	height := len(*heightmap)
+	width := len((*heightmap)[0])
 
 	if y == 0 {
-		neighbours = append(neighbours, heightmap[y+1][x])
+		neighbours = append(neighbours, point{y: y + 1, x: x, value: (*heightmap)[y+1][x]})
 	} else if y == height-1 {
-		neighbours = append(neighbours, heightmap[y-1][x])
+		neighbours = append(neighbours, point{y: y - 1, x: x, value: (*heightmap)[y-1][x]})
 	} else {
-		neighbours = append(neighbours, heightmap[y+1][x])
-		neighbours = append(neighbours, heightmap[y-1][x])
+		neighbours = append(neighbours, point{y: y + 1, x: x, value: (*heightmap)[y+1][x]})
+		neighbours = append(neighbours, point{y: y - 1, x: x, value: (*heightmap)[y-1][x]})
 	}
 
 	if x == 0 {
-		neighbours = append(neighbours, heightmap[y][x+1])
+		neighbours = append(neighbours, point{y: y, x: x + 1, value: (*heightmap)[y][x+1]})
 	} else if x == width-1 {
-		neighbours = append(neighbours, heightmap[y][x-1])
+		neighbours = append(neighbours, point{y: y, x: x - 1, value: (*heightmap)[y][x-1]})
 	} else {
-		neighbours = append(neighbours, heightmap[y][x+1])
-		neighbours = append(neighbours, heightmap[y][x-1])
+		neighbours = append(neighbours, point{y: y, x: x + 1, value: (*heightmap)[y][x+1]})
+		neighbours = append(neighbours, point{y: y, x: x - 1, value: (*heightmap)[y][x-1]})
 	}
 
 	return neighbours
 }
 
-func part2(heightmap [][]int) int {
-	return 2
+func contains(elements *[]string, elementToCheck string) bool {
+	if elements == nil {
+		return false
+	}
+
+	for i := range *elements {
+		if (*elements)[i] == elementToCheck {
+			return true
+		}
+	}
+	return false
 }
 
-func part1(heightmap [][]int) int {
+func markAsVisited(visited *[]string, p point) *[]string {
+	pointToMark := string(p.y) + "," + string(p.x)
+	if visited == nil {
+		visited = &[]string{pointToMark}
+		return visited
+	}
+
+	if !contains(visited, pointToMark) {
+		*visited = append(*visited, pointToMark)
+	}
+
+	return visited
+}
+
+func getBasinSize(heightmap *[][]int, startingPoint point, visited *[]string) int {
+	size := 0
+	for _, p := range startingPoint.neighbours {
+		if contains(visited, string(p.y)+","+string(p.x)) {
+			continue
+		}
+
+		if p.value > startingPoint.value && p.value < 9 {
+			visited = markAsVisited(visited, p)
+			size++
+
+			if p.neighbours == nil {
+				p.neighbours = getNeighbours(heightmap, p.y, p.x)
+			}
+
+			size += getBasinSize(heightmap, p, visited)
+		}
+	}
+
+	return size
+}
+
+func getLowPoints(heightmap [][]int) []point {
 	height := len(heightmap)
 	width := len(heightmap[0])
 
-	lowPoints := []int{}
+	lowPoints := []point{}
 
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
 			pointHeight := heightmap[y][x]
-			neighbours := getNeighbours(heightmap, y, x)
+			neighbours := getNeighbours(&heightmap, y, x)
 
 			if isLowPoint(pointHeight, neighbours) {
-				lowPoints = append(lowPoints, pointHeight)
+				lowPoints = append(lowPoints, point{y: y,
+					x:          x,
+					value:      pointHeight,
+					neighbours: neighbours,
+				})
 			}
 		}
 	}
+
+	return lowPoints
+}
+
+func part2(heightmap [][]int) int {
+
+	lowPoints := getLowPoints(heightmap)
+
+	basinSizes := []int{}
+	for _, p := range lowPoints {
+		basinSizes = append(basinSizes, getBasinSize(&heightmap, p, nil)+1)
+	}
+
+	sort.Ints(basinSizes)
+
+	len := len(basinSizes)
+	return basinSizes[len-1] * basinSizes[len-2] * basinSizes[len-3]
+}
+
+func part1(heightmap [][]int) int {
+	lowPoints := getLowPoints(heightmap)
 
 	return getRiskLevel(lowPoints)
 }
